100a1db8ab6656ea9476345d2a58a267865bf1af
Robert Horrigan <roberthorrigan@gmail.com>
2014-07-22 15:25:11 -0400

    Redirect user to user/show after logging in. Displays users name in flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.from_omniauth(env['omniauth.auth'])
     session[:user_id] = @user.id
-    redirect_to user_index_path
-    flash[:notice] = "You did it!"
+    redirect_to user_show_path
+    flash[:notice] = "You are signed in as #{@user.name}"
   end
 
 end
