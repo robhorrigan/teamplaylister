467279b7e25c6799ea3e83195b57b9489158a8db
Robert Horrigan <roberthorrigan@gmail.com>
2014-07-23 14:19:02 -0400

    Playlists index view JSON parsed to display playlists names only

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,7 @@
 class PlaylistsController < ApplicationController
   def index
-    @user_playlists = RestClient.get('https://api.spotify.com/v1/users/1249884999/playlists', { "Authorization" => "Bearer BQDRVCoxVkx_q9sUa-P7paM76PQvfAZnVbABHkxlAYgRJ4TxrleO5QESpvY1C8iL8CCkVsYP2xaec8QldxLNHRZ8xY0l7V4g9bdtPkR8SawBErvqRbjdMB4xg_0Qh_t8P6DXkJlb8O7O-DQe-pbQ_tqz6pcoHGc1pONyjA0jFukTxf6TMxyvY6uQdliND2ftj7zGO6_cDon3BGtV"})
+    user_playlists = RestClient.get('https://api.spotify.com/v1/users/1249884999/playlists', { "Authorization" => "Bearer BQDRVCoxVkx_q9sUa-P7paM76PQvfAZnVbABHkxlAYgRJ4TxrleO5QESpvY1C8iL8CCkVsYP2xaec8QldxLNHRZ8xY0l7V4g9bdtPkR8SawBErvqRbjdMB4xg_0Qh_t8P6DXkJlb8O7O-DQe-pbQ_tqz6pcoHGc1pONyjA0jFukTxf6TMxyvY6uQdliND2ftj7zGO6_cDon3BGtV"})
+    @my_list = JSON.parse(user_playlists)
   end
 
   def create
