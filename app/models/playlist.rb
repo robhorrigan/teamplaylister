cf1fe9d31e390db088cb85e7efe446aaf4ade07f
Kate Brender <kate.brender@gmail.com>
2014-07-23 12:13:18 -0400

    started on playlist model

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,9 @@
 class Playlist < ActiveRecord::Base
-	belongs_to: :party
-	belongs_to: :playlist, through: :parties
+	belongs_to :party
+	belongs_to :user
+
+	attr_accessor :party, :name, :votes
+	def initialize(party, name, votes)
+		@code = rand(36**5).to_s(36)
+	end
 end
