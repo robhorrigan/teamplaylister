0d00b2a4b73d56164f47717abac80f8bd5464a18
christinebeaubrun <christinebeaubrun@gmail.com>
2014-07-23 13:40:09 -0400

    Can find and create playlists

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.from_omniauth(env['omniauth.auth'])
+    binding.pry
     session[:user_id] = @user.id
     redirect_to user_show_path
     flash[:notice] = "You are signed in as #{@user.name}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,25 @@ class User < ActiveRecord::Base
 	# has_many: :playlists, through: :parties
   
   def self.from_omniauth(auth)
-    where(auth.slice('uid')).first || create_from_omniauth(auth)
-  end
-
-  def self.create_from_omniauth(auth)
-    create! do |user|
+    # where(auth.slice('uid')).first || create_from_omniauth(auth)
+    where(auth.slice('uid')).first_or_initialize.tap do |user|
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
       user.token = auth["credentials"]["token"]
       user.refresh_token = auth["credentials"]["refresh_token"]
       user.image = auth["info"]["image"]
-    end
+      user.save
+    end 
   end
+
+  # def self.create_from_omniauth(auth)
+  #   create! do |user|
+  #     user.uid = auth["uid"]
+  #     user.name = auth["info"]["name"]
+  #     user.token = auth["credentials"]["token"]
+  #     user.refresh_token = auth["credentials"]["refresh_token"]
+  #     user.image = auth["info"]["image"]
+  #   end
+  # end
 
 end
