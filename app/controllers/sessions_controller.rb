c1366c87c87736aa4d79eebb4076b2dcd89ffd7f
Robert Horrigan <roberthorrigan@gmail.com>
2014-07-23 13:07:54 -0400

    User model now updates user info on each log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.from_omniauth(env['omniauth.auth'])
-    binding.pry
+    @user = User.from_omniauth(env['omniauth.auth']) 
     session[:user_id] = @user.id
     redirect_to user_show_path
     flash[:notice] = "You are signed in as #{@user.name}"
