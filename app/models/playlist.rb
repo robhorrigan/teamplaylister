0771f6aeadc937e1d4681b229c6d105c5a452005
christinebeaubrun <christinebeaubrun@gmail.com>
2014-07-23 10:47:29 -0400

    Working on playlist creation

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,19 @@
+
 class Playlist < ActiveRecord::Base
 	belongs_to: :party
 	belongs_to: :playlist, through: :parties
+
+  RSpotify.authticate("a3d2521b73de4bcc87357ce82f8dbd0c", "2eaee1b3f89244c497a04b1c0d0491b3")
+
+  def new_spotify_user
+    spotify_user = RSpotify::User.new()
+
+  def self.new_playlist(name)
+    playlist = user.create_playlist!(name)
+  end
+
+  def self.new_tracks(tracks_array)
+    playlist.add_tracks!(tracks)
+  end
+
 end
