0ef96efc41e35055779021782331e7767666945a
christinebeaubrun <christinebeaubrun@gmail.com>
2014-07-24 10:56:39 -0400

    We can add tracks

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,14 +4,9 @@ class Playlist < ActiveRecord::Base
 	belongs_to :user
 
 	attr_accessor :party, :name, :votes, :code
-	def initialize(party, name, votes)
-		@code = rand(36**5).to_s(36)
-	end
-
   # def self.uri_encode_parse(uid, playlist_name, token)
 
   def self.create_playlist(uid, playlist_name, token)
-    # binding.pry
     RestClient.post("https://api.spotify.com/v1/users/#{uid}/playlists", {name: "#{playlist_name}", public: false}.to_json, {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}"})
   end
 
@@ -21,13 +16,13 @@ class Playlist < ActiveRecord::Base
   end
 
   def self.add_tracks(uid, playlist_id,token)
-    # binding.pry
     encoded_url = URI.encode('uris=spotify:track:0mBkoM8r7KAQzZij5swTUL%2Cspotify:track:15uFqHGAOvaGCqikvgiW6w')
-    request = Typhoeus::Request.new(
-      "https://api.spotify.com/v1/users/#{uid}/playlists/#{playlist_id}/tracks?#{encoded_url}",
-      {:method => :post,
-      :headers => { "Content-Type" => "application/json", "Authorization" => "Bearer #{token}" }}
-    )
+    RestClient.post("https://api.spotify.com/v1/users/#{uid}/playlists/#{playlist_id}/tracks", ["spotify:track:0mBkoM8r7KAQzZij5swTUL", "spotify:track:15uFqHGAOvaGCqikvgiW6w"].to_json, {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}"})
+    # request = Typhoeus::Request.new(
+    #   "https://api.spotify.com/v1/users/#{uid}/playlists/#{playlist_id}/tracks?#{encoded_url}",
+    #   {:method => :post,
+    #   :headers => { "Content-Type" => "application/json", "Authorization" => "Bearer #{token}" }}
+    # )
   end
 
 end
