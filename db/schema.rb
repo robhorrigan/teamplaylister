a57722617e3e930245f93f5e1e12a867290e7a28
jasperknowsbest <edward.jasper@flatironschool.com>
2014-08-23 13:38:49 -0400

    lookin good

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140731193806) do
+ActiveRecord::Schema.define(version: 20140823173547) do
 
   create_table "parties", force: true do |t|
     t.integer  "user_id"
@@ -50,11 +50,6 @@ ActiveRecord::Schema.define(version: 20140731193806) do
     t.text     "image"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "votes", force: true do |t|
-    t.string "party_code"
-    t.string "ip"
   end
 
 end
