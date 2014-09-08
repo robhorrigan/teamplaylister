f880b9be92e73318a7e4b48f3003256b66b39006
jasperknowsbest <edward.jasper@flatironschool.com>
2014-09-08 15:53:21 -0400

    party validations done

--- spec/factories/parties.rb
+++ spec/factories/parties.rb
@@ -1,21 +1,24 @@
 FactoryGirl.define do
   factory :party do |f|
-    f.name { Faker::Lorem.word(10) }
-    f.name { Faker::Name.name }
-    f.token { Faker::Lorem.characters(220) }
-    f.refresh_token { Faker::Lorem.characters(131) } 
+    f.name { Faker::Lorem.word }
+    f.code { Faker::Lorem.characters(5) }
+    f.user_id { Faker::Lorem.characters(10) }
+    f.spotify_playlist_id { Faker::Lorem.characters(22) }
   end
 
-  trait :name do
+  trait :party_name do
+    Faker::Lorem.word
+  end
+
+  trait :code do
+    Faker::Lorem.characters(5)
+  end
+
+  trait :user_id do
     Faker::Lorem.characters(10)
   end
-  trait :name do
-    Faker::Name.name
-  end
-  trait :token do
-    Faker::Lorem.characters(220)
-  end
-  trait :refresh_token do
-    Faker::Lorem.characters(131)
+
+  trait :spotify_playlist_id do
+    Faker::Lorem.characters(22)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,8 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
 
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
